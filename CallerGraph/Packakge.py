f6e9a9911fce142d24ed4d1193c342c3da084b25
Jason Li <jason.li@kitware.com>
2011-10-28 09:43:25 -0400

    Working Dot file generator and png files

--- CallerGraph/Packakge.py
+++ CallerGraph/Packakge.py
@@ -22,6 +22,8 @@ import re
 import os
 import os.path
 import sys
+import subprocess
+
 from datetime import datetime, date, time
 
 #Routines starts with A followed by a number
@@ -105,35 +107,6 @@ class Routine:
                 write ("Package: %s \n" % routinePackageMap[var])
             else:
                 write("\n")
-    # print routine call graph in the graphviz dot format
-    def outputToDot(self):
-
-        if len(self.calledRoutines) == 0:
-            print("No called Routines found! for routine:%s") % (self.name)
-            return
-        localPackage=dict()
-        localPackage[routinePackageMap[self.name]]=set()
-        localPackage[routinePackageMap[self.name]].add(self.name)
-        for var in self.calledRoutines:
-            if var in routinePackageMap.keys():
-                if not localPackage[routinePackageMap[var]]:
-                    localPackage[routinePackageMap[var]]=set()
-                localPackage[routinePackageMap[var]].add(var)
-        try:
-            dirName=("c:/temp/VistA/%s/") % routinePackageMap[self.name]
-            if not os.path.exists(dirName):
-                os.makedirs(dirName)
-        except OSError:
-            print "Error making dir %s : Error: %s"  % (dirName, OSError)
-            return
-        output=open(("c:\temp\VistA/%s/%s.gv")%(routinePackageMap[self.name], self.name),'w')
-        output.write("digraph {\n")
-        for var in localPackage.key():
-            pass
-        
-        
-        outout.write("}\n")
-        
     def printResultInC(self):
         try:
             dirName=("c:/temp/VistA/%s/") % routinePackageMap[self.name]
@@ -182,7 +155,9 @@ class Routine:
         file.write("}\n")        
         file.write("}// end of namespace")
         file.close()    
-        
+        #subprocess to write to the ps file
+
+    
 class Package:
     #constructor
     def __init__(self, packageName):
@@ -265,8 +240,79 @@ globalVarParser=GlobalVarSectionParse()
 nakedGlobalParser=NakedGlobalsSectionParser()
 markedItemsParser=MarkedItemsSectionParser()
 calledRoutineParser=CalledRoutineSectionParser()
+#===============================================================================
+# interface to generated the output based on a routine
+#===============================================================================
+class RoutineVisit:
+    def visitRoutine(self, routine, outputDir):
+        pass
+#===============================================================================
+# Default implementation of the routine 
+#===============================================================================    
+class DefaultRoutineVisit:
+    def visitRoutine(self, routine,outputDir):
+        routine.printResult()
+#===============================================================================
+# output result to use graphviz to generated the graph
+#===============================================================================
+class GraphvizRoutineVisit(RoutineVisit):
+    def visitRoutine(self,routine, outputDir):
+        routineName=routine.getName()
+        calledRoutines=routine.getCalledRoutines()
+        packageName = routinePackageMap[routineName]
+        if len(calledRoutines) == 0:
+            print("No called Routines found! for routine:%s") % (routineName)
+            return
+        localPackage=dict()
+        localPackage[routinePackageMap[routineName]]=set()
+        localPackage[routinePackageMap[routineName]].add(routineName)
+        for var in calledRoutines:
+            if var in routinePackageMap.keys():
+                if routinePackageMap[var] not in localPackage.keys():
+                    localPackage[routinePackageMap[var]]=set()
+                localPackage[routinePackageMap[var]].add(var)
+        
+        try:
+            dirName=os.path.join(outputDir,packageName)
+            if not os.path.exists(dirName):
+                os.makedirs(dirName)
+        except OSError:
+            print "Error making dir %s : Error: %s"  % (dirName, OSError)
+            return
+        output=open(os.path.join(dirName,routineName+".dot"),'w')
+        output.write("digraph %s {\n" % routineName)
+        if packageName not in localPackage.keys():
+            output.write("\tsubgraph %s{\n" % (var.replace(' ','_').replace('-','_')))
+            output.write("\t\t" + routineName + ";\n")
+            output.write("\t\tlabel=\"%s\";" % var)
+            output.write("\t}\n")
+        for var in localPackage.keys():
+            output.write("\tsubgraph %s{\n" % (var.replace(' ','_').replace('-','_')))
+            if var == packageName:
+                output.write("\t\t%s->{" % routineName)
+                for val in localPackage[var]:
+                    if val != routineName:
+                        output.write(val + ";")
+                output.write("}\n")
+            else:
+                for val in localPackage[var]:
+                    output.write("\t\t" + val+";\n")
+            output.write("\t\tlabel=\"%s\";\n" % var)
+            output.write("\t}\n")
+            if var != packageName:
+                for val in localPackage[var]:
+                    output.write("\t" + routineName + "->" + val + ";\n")
+        output.write("}\n")
+        output.close()
 
-
+        outputName = os.path.join(dirName,routineName+".png")
+        inputName=os.path.join(dirName,routineName+".dot")
+        command="dot -Tpng -o \"%s\" \"%s\"" % (outputName, inputName)
+#        print command
+        retCode=subprocess.call(command)
+        if retCode != 0:
+            print "calling dot returns %d" % retCode
+                
 class CallerGraphLogFileParser:
     def __init__(self):
         self.allRoutines = dict()
@@ -305,12 +351,15 @@ class CallerGraphLogFileParser:
             self.parser.parseLine(line)
     def printResult(self):
         print "Total Routines are %d" % len(self.allRoutines)  
-    def printRoutine(self, routineName): 
+    def printRoutine(self, routineName, visitor=DefaultRoutineVisit()): 
         if routineName in self.allRoutines.keys():
 #            self.allRoutines[routineName].printResult()
-            self.allRoutines[routineName].printResultInC()
+#            self.allRoutines[routineName].printResultInC()
+            routine=self.allRoutines[routineName]
+            visitor.visitRoutine(routine,"C:/Temp/VistA/")
         else:
-            print "Routine: %s Not Found!" % routineName
+            pass
+#            print "Routine: %s Not Found!" % routineName
 
 
 def findPackagesAndRoutinesBySource(dirName, pattern):
@@ -346,31 +395,24 @@ def parseAllCallerGraphLog(dirName, pattern):
             result = RoutineStart.search(line)
             if result:
                 routineName = result.group('name')
-    #            print "routine %s Started:" % routineName
                 logParser.onNewRoutineStart(routineName)
                 continue
             if localVarStart.search(line):
-    #            print "localVar Started:"
                 logParser.onLocalVariablesStart(line)
                 continue
             if globalVarStart.search(line):
-    #            print "global started:"
                 logParser.onGlobaleVariables(line)
                 continue
             if nakedGlobalStart.search(line):
-    #            print "naked global started"
                 logParser.onNakedGlobals(line)
                 continue
             if markedItemStart.search(line):
-    #            print "marked Item started"
                 logParser.onMarkedItems(line)
                 continue
             if calledRoutineStart.search(line):
-    #            print "called routine started"
                 logParser.onCalledRoutines(line)
                 continue
             if RoutineEnd.search(line):
-    #            print "routine End called"
                 logParser.onNewRoutineEnd(routineName)
                 continue
             if routineInvokesStart.search(line):
@@ -382,12 +424,24 @@ def parseAllCallerGraphLog(dirName, pattern):
                 logParser.parseNameValuePair(line)
                 continue
     logParser.printResult()
+    
+def testDotCall():
+    packageName="Nursing Service"
+    routineName="NURA6F1"
+    dirName = os.path.join("c:/temp/VistA/", packageName);
+    outputName = os.path.join(dirName,routineName+".png")
+    inputName=os.path.join(dirName,routineName+".dot")
+    command="dot -Tpng -o \"%s\" \"%s\"" % (outputName, inputName)
+    retCode=subprocess.call(command)
+    print "calling dot returns %d" % retCode
       
 if __name__ == '__main__':
     # the step to parse the log file
     #parse the log file
+#    testDotCall()
+#    exit()
     callLogDir = "C:/Users/jason.li/build/VistaCache/Docs/CallerGraph"
-    callLogPattern="Toolkit.log"
+    callLogPattern="Nursing_Service.log"
     print "Starting Parsing all files...."
     print "Time is: %s" % datetime.now()
     parseAllCallerGraphLog(callLogDir, callLogPattern)
@@ -397,7 +451,7 @@ if __name__ == '__main__':
     routineFileDir = "C:/cygwin/home/jason.li/git/VistA-FOIA/Packages/"
     findPackagesAndRoutinesBySource(routineFileDir, routineFilePattern)
     for var in routinePackageMap.keys():
-        logParser.printRoutine(var)
+        logParser.printRoutine(var, GraphvizRoutineVisit())
     # read the user input from the terminal
     isExit=False
     while not isExit:
